604be2abbc870d823fc02b43f92ae6f027a0b957
alterPublics <jakobbk@ruc.dk>
2025-07-10 08:42:13 +0200

    fixed minor setting

--- day2day/data/market_data.py
+++ day2day/data/market_data.py
@@ -237,11 +237,11 @@ class MarketDataCollector:
             
             # Check token validity every hour
             current_time = time.time()
-            if current_time - last_token_check > token_check_interval:
-                logger.info("Checking token validity during long collection process")
-                if not self.authenticator.ensure_valid_token():
-                    raise Exception("Failed to maintain valid access token during collection")
-                last_token_check = current_time
+            #if current_time - last_token_check > token_check_interval:
+            logger.info("Checking token validity during long collection process")
+            if not self.authenticator.ensure_valid_token():
+                raise Exception("Failed to maintain valid access token during collection")
+            last_token_check = current_time
             
             try:
                 instrument_data = self.collect_instrument_data(
